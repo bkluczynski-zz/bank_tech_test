4ecb3e3da81386c8e7a9e18ffc77035cfb65212b
Bart <kluczynskib@gmail.com>
2016-09-21 11:30:44 +0100

    implemented guard clauses for withdrawing when balance is 0 or less and for value which is not a positive integer

--- lib/account.rb
+++ lib/account.rb
@@ -10,14 +10,24 @@ end
 
 
 def deposit(amount)
+positive_integer(amount)
 @balance += amount
 end
 
 def withdraw(amount)
+positive_integer(amount)
+fail 'Not enough funds to proceed with operation' if @balance - amount < 0
 @balance -= amount
 end
 
 
 
+
+private
+
+
+def positive_integer(amount)
+fail 'Please insert the correct value' if amount <= 0
+end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,22 +7,36 @@ require 'account'
 
         describe 'operations on the bank account' do
           context 'deposit' do
-          it 'increases the balance by the amount of deposit' do
-          subject.deposit(50)
+            it 'increases the balance by the amount of deposit' do
+              subject.deposit(50)
           expect(subject.balance).to eq 50
         end
       end
 
         context 'withdrawal' do
           it 'decreases the balance by the amount of withdrawal' do
-          subject.deposit(30)
-          subject.withdraw(30)
+            subject.deposit(30)
+            subject.withdraw(30)
           expect(subject.balance).to eq 0
         end
       end
 
+        context 'when user does not have enough to withdraw the desired amount' do
+          it 'gives a user the  error message' do
+          expect { subject.withdraw(10) }.to raise_error 'Not enough funds to proceed with operation'
+          end
+        end
+
+        context 'when user type the negative value to deposit or withdraw' do
+          it 'gives a user the error message' do
+            expect { subject.deposit(-10) }.to raise_error 'Please insert the correct value'
+            expect { subject.withdraw(-10) }.to raise_error 'Please insert the correct value'
+          end
+        end
 
   end
+
+
 
 
 
